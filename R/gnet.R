c19e5d19a7958d98c0552db0f49056e45f46bb11
Pariya Behrouzi <pariya_behrouzi@yahoo.com>
2012-10-24 16:41:53 +0200

    Separate out the calculate.Theta function
    Before we can do multicore processing we need a clean function call on a
    single core. This will alllow us to do much more when going into
    multicore programming

--- R/gnet.R
+++ R/gnet.R
@@ -8,32 +8,36 @@
 # gnet
 #
 
-gnet = function(y, n.rho=10, em.iter=10, gibbs.iter=1000){
-  begTime <- Sys.time()
+gnet = function(y, n.rho=10, em.iter=10, gibbs.iter=1000, n.cores=1){
   cat("Starting with Gnet\n")
   p = ncol(y)
   rho = seq(0.01,.9,length=n.rho)
   cutoffs = calculate.cutoffs(y)
   lower.upper = calculate.lower.upper(y, cutoffs)
   cat("Cut-offs calculated\n")
-  results = list(Theta=list(), loglik=NULL, rho=rho, begTime=begTime, endTime=NULL, runTime=NULL)
-  
-  for (i in 1:n.rho){ #PARRALEL  20 x NODES
-    cat("Calculation of Rho", i,"\n")
-    sigma.ini = diag(rep(1,p))
-    for (j in 1:em.iter){
-      s <- proc.time()
-      cat("Start of iteration",j,"\n")
-      R    <- calculate.R(y, sigma.ini, lower.upper, gibbs.iter)
-      R.gl <- glasso(R, rho[i], penalize.diagonal=FALSE)
-      sigma.ini <- R.gl$w
-      cat("Iteration",j,"completed after",(proc.time()-s)[3],"seconds\n")
+  results = list(Theta=list(), loglik=NULL, rho=rho)
+  if(n.cores==1){
+    for(i in 1:n.rho){ #PARRALEL  20 x NODES
+      R.gl <- calculate.Theta(i,y,rho,lower.upper, em.iter, gibbs.iter)
+      results$Theta[[i]] <- Matrix(R.gl$wi)
+      results$loglik     = c(results$loglik,R.gl$loglik)
     }
-    results$Theta[[i]] <- Matrix(R.gl$wi)
-    results$loglik     <- c(results$loglik,R.gl$loglik)
-    results$endTime    <- Sys.time()
-    results$runTime    <- results$endTime - begTime    
-    cat("Rho",i,"completed\n")
   }
   return(results)
+}
+
+calculate.Theta <- function(i, y, rho, lower.upper, em.iter = 10, gibbs.iter=1000){
+  cat("Calculation of Rho", i,"\n")
+  p = ncol(y)
+  sigma.ini = diag(rep(1,p))
+  for (j in 1:em.iter){
+    s <- proc.time()
+    cat("Start of iteration",j,"\n")
+    R    <- calculate.R(y, sigma.ini, lower.upper, gibbs.iter)
+    R.gl <- glasso(R, rho[i], penalize.diagonal=FALSE)
+    sigma.ini <- R.gl$w
+    cat("Iteration",j,"completed after",(proc.time()-s)[3],"seconds\n")
+  }
+  cat("Rho",i,"completed\n")
+  return(R.gl)
 }
